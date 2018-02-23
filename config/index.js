cff3a3f46a5e755177c8430f4f6ec36a45306c6b
yanglx <1071126434@qq.com>
2018-02-23 21:03:16 +0800

    dabao

--- config/index.js
+++ config/index.js
@@ -1,34 +1,35 @@
-'use strict'
+"use strict";
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   dev: {
     // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: "static",
+    assetsPublicPath: "/",
     proxyTable: {
-      '/api': {
-        target: 'http://182.61.29.51:8089', // 测试环境
-        // target: 'http://182.61.24.42:8089', // 生产环境
+      "/api": {
+        // target: 'http://182.61.29.51:8089', // 测试环境
+        target: "http://18.218.181.216:8089", // 生产环境
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
+          "^/api": ""
         }
       },
-      '/ipApi': { // 获取ip地址
-        target: 'http://freegeoip.net/json/',
+      "/ipApi": {
+        // 获取ip地址
+        target: "http://freegeoip.net/json/",
         changeOrigin: true,
         pathRewrite: {
-          '^/ipApi': ''
+          "^/ipApi": ""
         }
       }
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: "localhost", // can be overwritten by process.env.HOST
     // host: '0.0.0.0',
     port: 8022, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
@@ -49,7 +50,7 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
+    devtool: "cheap-module-eval-source-map",
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
@@ -61,12 +62,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, "../dist/index.html"),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, "../dist"),
+    assetsSubDirectory: "static",
+    assetsPublicPath: "/",
 
     /**
      * Source Maps
@@ -74,14 +75,14 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
+    devtool: "#source-map",
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
+    productionGzipExtensions: ["js", "css"],
 
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
@@ -89,4 +90,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
--- station.js
+++ station.js
@@ -1,34 +1,42 @@
-var express = require('express')
-var proxy = require('http-proxy-middleware')
-var history = require('connect-history-api-fallback')
-var compression = require('compression')
-var app = express()
-app.use(compression())
-app.use('/api', proxy({
-  target: 'http://182.61.29.51:8089',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': ''
-  }
-}))
-app.use('/ipApi', proxy({
-  target: 'http://freegeoip.net/json/',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/ipApi': ''
-  }
-}))
-app.use(history({
-  index: './index.html'
-}))
-app.use(express.static('./dist'))
-  // var port = process.env.PORT || config.build.port
-  // 生产环境的端口号
-var port = 8022
+var express = require("express");
+var proxy = require("http-proxy-middleware");
+var history = require("connect-history-api-fallback");
+var compression = require("compression");
+var app = express();
+app.use(compression());
+app.use(
+  "/api",
+  proxy({
+    target: "http://18.218.181.216:8089",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": ""
+    }
+  })
+);
+app.use(
+  "/ipApi",
+  proxy({
+    target: "http://freegeoip.net/json/",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/ipApi": ""
+    }
+  })
+);
+app.use(
+  history({
+    index: "./index.html"
+  })
+);
+app.use(express.static("./dist"));
+// var port = process.env.PORT || config.build.port
+// 生产环境的端口号
+var port = 8022;
 module.exports = app.listen(port, function(err) {
   if (err) {
-    console.log(err)
-    return
+    console.log(err);
+    return;
   }
-  console.log('Listening at http://localhost:' + port + '\n')
-})
+  console.log("Listening at http://localhost:" + port + "\n");
+});
