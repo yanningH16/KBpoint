ba43b9cd73e0bc1166e7dadd81197e3a81df268a
yanglx <1071126434@qq.com>
2018-03-05 19:45:37 +0800

    bug

--- config/index.js
+++ config/index.js
@@ -30,7 +30,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: "localhost", // can be overwritten by process.env.HOST
-    // host: '0.0.0.0',
+    // host: "0.0.0.0",
     port: 8022, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
